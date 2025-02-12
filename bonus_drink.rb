c512ad2e9c6c4a4ad4f1b32fefd95088c89f71e8
ri226ku <ri218718u@icloud.com>
2025-02-12 20:42:49 +0900

    Update bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,19 @@
-class BonusDrink
-  def self.total_count_for(amount)
-    0
+buy = gets.to_i  # 標準入力を整数に変換
+present = 0
+flg = 0
+
+flg = 0
+if buy >= 3
+  drank = buy
+  flg = 1
+  while flg==1 do
+    get = (drank / 3).floor
+    present = present + get
+    leave = drank % 3
+    drank = get + leave
+    if drank < 3
+      flg=0
+    end
   end
 end
+puts buy + present
